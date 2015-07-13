e41ccda441cf181b27592344ac6e292fb9fbcf23
Michael Pope <mpope.cr@gmail.com>
2015-07-13 17:02:45 -0400

    Add basic search form

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,6 @@
+require "open-uri"
+
+class SearchController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root to: "search#index"
+
   namespace :api do
     namespace :v1 do
       get "/search", to: "search#index"
     end
   end
+
+  get "/search", to: "search#index"
 end
