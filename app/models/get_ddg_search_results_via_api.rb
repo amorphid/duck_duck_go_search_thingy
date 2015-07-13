94d99b6bffb3178c5a0ba0f2594f0691f6a3158f
Michael Pope <mpope.cr@gmail.com>
2015-07-13 12:43:28 -0400

    Refactor GetDDGSearchResultsViaApi to be more functional

--- app/models/get_ddg_search_results_via_api.rb
+++ app/models/get_ddg_search_results_via_api.rb
@@ -1,53 +1,78 @@
 require "open-uri"
 
 class GetDDGSearchResultsViaApi
-  attr_reader :response, :uri
 
-  def get_result
+  public
+
+  def search(query)
+    contents_as_json = fetch_contents(uri(query))
+    return nil unless topic_summary?(contents_as_json["Type"])
+    contents_to_hash(contents_as_json)
+  end
+
+  private
+
+  def content_to_hash(content)
     {
-      infobox: response["Infobox"],
-      results: parse_results,
-      type: response["Type"]
+      href:        href(href_node(content)),
+      description: description(content)
     }
   end
 
-  def parse_results
-    response["RelatedTopics"].map do |topic|
-      a_node = Nokogiri::HTML(topic["Result"]).css("a")
+  def contents_to_hash(contents)
+    {
+      infobox: infobox(contents["Infobox"]),
+      results: results(contents["RelatedTopics"]),
+      type:    type(contents["Type"])
+    }
+  end
 
-      href = {}
-      href[:title] = a_node.children.map { |c| c.text.strip }.join(" ")
-      href[:url]   = a_node.attribute("href").value
+  def description(content)
+    content["Text"]
+  end
 
-      description = topic["Text"]
+  def fetch_contents(uri)
+    JSON.parse(uri.read)
+  end
 
-      { href: href, description: description }
+  def href(node)
+    {
+      title: href_title(node),
+      url:   href_url(node)
+    }
+  end
+
+  def href_node(content)
+    Nokogiri::HTML(content["Result"]).css("a")
+  end
+
+  def href_title(node)
+    node.children.map { |c| c.text.strip }.join(" ")
+  end
+
+  def href_url(node)
+    node.attribute("href").value
+  end
+
+  def infobox(infobox)
+    infobox.empty? ? {} : infobox
+  end
+
+  def results(contents)
+    contents.map do |content|
+      content_to_hash(content)
     end
   end
 
-  def search(query)
-    # set url for API call
-    set_uri(query)
-
-    # query the DDG API
-    set_response
-
-    #  Response MUST have a topic summary
-    return nil unless topic_summary?
-
-    get_result
+  def topic_summary?(type)
+    type == "A"
   end
 
-  def set_response
-    @response = JSON.parse(uri.read)
+  def type(type)
+    type.blank? ? "" : type
   end
 
-  def set_uri(query)
-    @uri = URI("https://api.duckduckgo.com/?q=#{query}&format=json&pretty=1")
-  end
-
-  # Response from DDG has topic summaries if it has Type A
-  def topic_summary?
-    response["Type"] == "A"
+  def uri(query)
+    URI("https://api.duckduckgo.com/?q=#{query}&format=json&pretty=1")
   end
 end
